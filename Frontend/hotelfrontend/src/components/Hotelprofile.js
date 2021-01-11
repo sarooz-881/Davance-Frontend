33c6838767f681a9d68aff49d08fb8669b21ad6b
unknown <iambruce1010@example.com>
2021-01-11 12:33:50 +0545

    api updated

--- Frontend/hotelfrontend/src/components/Hotelprofile.js
+++ Frontend/hotelfrontend/src/components/Hotelprofile.js
@@ -1,71 +1,311 @@
 import React, { Component } from "react";
 import "../css/main.css";
-import {Alert} from "reactstrap";
+import { Alert, Button, Input } from "reactstrap";
+import Axios from "axios";
 
 class Hotelprofile extends Component {
-    render() {
-        return(
-            <div className="profile">
-                <div className="container-fluid col-lg-12">
-                <div className="tab-pane ">
-                <div className="row">
-                                            <div className="col-md-12">
-                                                <label>Hotel Name</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p></p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>Email</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p></p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-12">
-                                                <label>First Name</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p></p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-12">
-                                                <label>Last Name</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p></p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>Mobile</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p></p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>Address</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p></p>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                                <label>Role</label>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <p></p>
-                                            </div>
-                                        </div>
-                            </div>
-                </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      hotelExists: false,
+      hotelID: "",
+      hotelInfo: [],
+      hotelEmail: "",
+      hotelName: "",
+      hotelContact: "",
+      hotelDescription: "",
+      config: {
+        headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+      },
+
+      country: "",
+      state: "",
+      street: "",
+      Name: "",
+      email: "",
+      mobile: ""
+    };
+  }
+  componentDidMount = () => {
+    Axios.get("http://localhost:3005/ehotel/hotel", this.state.config)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.length === 0) {
+          this.setState({ hotelExists: false });
+        } else {
+          this.setState({
+            hotelEmail: res.data[0].email,
+            hotelContact: res.data[0].contact,
+            hotelName: res.data[0].hotelName,
+            hotelDescription: res.data[0].description,
+            hotelInfo: res.data[0],
+            hotelID: res.data[0]._id,
+            hotelExists: true,
+            Name: res.data[0].hotelOwner[0].ownerName,
+            email: res.data[0].hotelOwner[0].email,
+            mobile: res.data[0].hotelOwner[0].contact,
+          });
+          if (res.data[0].address.length !== 0) {
+            this.setState({
+              country: res.data[0].address[0].country,
+              state: res.data[0].address[0].state,
+              street: res.data[0].address[0].street,
+            });
+          }
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.hotelExists) {
+      Axios.post(
+        "http://localhost:3005/ehotel/hotel",
+        {
+          email: this.state.hotelEmail,
+          hotelName: this.state.hotelName,
+          contact: this.state.hotelContact,
+          description: this.state.hotelDescription,
+        },
+        this.state.config
+      )
+        .then((res) => {
+          console.log(res.data);
+          this.setState({ hotelExists : true})
+          Axios.post(
+            `http://localhost:3005/ehotel/hotel/${res.data[0]._id}/address`,
+            {
+              country: this.state.country,
+              state: this.state.state,
+              street: this.state.street,
+            },
+            this.state.config
+          ).then((res) => {
+            console.log(res.data);
+          });
+        })
+        .catch((err) => console.log(err));
+    } else {
+      Axios.put(
+        `http://localhost:3005/ehotel/hotel/${this.state.hotelID}`,
+        {
+          email: this.state.hotelEmail,
+          hotelName: this.state.hotelName,
+          contact: this.state.hotelContact,
+          description: this.state.hotelDescription,
+        },
+        this.state.config
+      )
+        .then((res) => {
+          console.log(res.data);
+          Axios.post(
+            `http://localhost:3005/ehotel/hotel/${this.state.hotelID}/address`,
+            {
+              country: this.state.country,
+              state: this.state.state,
+              street: this.state.street,
+            },
+            this.state.config
+          ).then((res) => {
+            console.log(res.data);
+          });
+          Axios.post(
+            `http://localhost:3005/ehotel/hotel/${this.state.hotelID}/hotelOwner`,
+            {
+              ownerName: this.state.Name,
+              email: this.state.email,
+              contact: this.state.mobile,
+            },
+            this.state.config
+          ).then((res) => {
+            console.log(res.data);
+          });
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <div className="profile">
+        <div className="container-fluid col-lg-12">
+          <div className="tab-pane ">
+            <h2>Owner Information</h2>
+            <hr />
+
+            <div className="row">
+              <div className="col-md-6">
+                <label>Name</label>
+              </div>
+              <div className="col-md-6">
+              <Input
+                  id="Name"
+                  name="Name"
+                  placeholder="Enter Hotel owner name here...."
+                  value={this.state.Name}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
             </div>
-        )}}
-        export default Hotelprofile;
+
+            <div className="row">
+              <div className="col-md-6">
+                <label>Email</label>
+              </div>
+              <div className="col-md-6">
+              <Input
+                  id="email"
+                  name="email"
+                  placeholder="Enter owner email here...."
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-6">
+                <label>Mobile</label>
+              </div>
+              <div className="col-md-6">
+              <Input
+                  id="mobile"
+                  name="mobile"
+                  placeholder="Enter owner contact here...."
+                  value={this.state.mobile}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
+            </div>
+
+            <h2 style={{ marginTop: "2rem" }}>Hotel Information</h2>
+            <hr />
+            <div className="row">
+              <div className="col-md-6">
+                <label>Hotel Name</label>
+              </div>
+              <div className="col-md-6">
+                <Input
+                  id="hotelName"
+                  name="hotelName"
+                  placeholder="Enter Hotel name here...."
+                  value={this.state.hotelName}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <label>Hotel Email</label>
+              </div>
+              <div className="col-md-6">
+                <Input
+                  id="hotelEmail"
+                  name="hotelEmail"
+                  placeholder="Enter Hotel email here...."
+                  value={this.state.hotelEmail}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-6">
+                <label>Hotel Contact</label>
+              </div>
+              <div className="col-md-6">
+                <Input
+                  id="hotelContact"
+                  name="hotelContact"
+                  placeholder="Enter Hotel contact here...."
+                  value={this.state.hotelContact}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <label>Description</label>
+              </div>
+              <div className="col-md-6">
+                <Input
+                  id="hotelDescription"
+                  name="hotelDescription"
+                  placeholder="Enter Hotel Description here...."
+                  value={this.state.hotelDescription}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-6">
+                <label>Address</label>
+              </div>
+            </div>
+            <hr />
+            <div className="row">
+              <div className="col-md-6">
+                <label>Country</label>
+              </div>
+              <div className="col-md-6">
+                <Input
+                  id="country"
+                  name="country"
+                  placeholder="Choose country...."
+                  value={this.state.country}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <label>State</label>
+              </div>
+              <div className="col-md-6">
+                <Input
+                  id="state"
+                  name="state"
+                  placeholder="Choose state...."
+                  value={this.state.state}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <label>Street</label>
+              </div>
+              <div className="col-md-6">
+                <Input
+                  id="street"
+                  name="street"
+                  placeholder="Choose street...."
+                  value={this.state.street}
+                  onChange={this.handleChange}
+                ></Input>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Button
+            color="primary"
+            style={{
+              margin: "3rem",
+              padding: "0.7rem 1.2rem",
+              fontSize: "1.8rem",
+            }}
+            onClick={this.handleSubmit}
+          >
+            Update
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+export default Hotelprofile;
